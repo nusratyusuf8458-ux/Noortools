156cfdf90df9f74ec63ac220f2609f2d54b508e6
nusratyusuf8458-ux <nusratyusuf8458@gmail.com>
2026-09-14 11:30:03 -0700

    phase-3c: expose Hajj and Umrah foundation

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,15 @@ import './android.css'
 import './content.css'
 import './phase3.css'
 import './phase3b.css'
+import './phase3c.css'
 import App from './App'
 import Phase2Launcher from './Phase2Launcher'
 import Phase23Launcher from './Phase23Launcher'
 import GlobalSearchLauncher from './GlobalSearchLauncher'
 import Phase3Launcher from './Phase3Launcher'
 import Phase3BLauncher from './Phase3BLauncher'
+import Phase3CLauncher from './Phase3CLauncher'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /><GlobalSearchLauncher /><Phase3Launcher /><Phase3BLauncher /></></StrictMode>,
+  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /><GlobalSearchLauncher /><Phase3Launcher /><Phase3BLauncher /><Phase3CLauncher /></></StrictMode>,
 )
