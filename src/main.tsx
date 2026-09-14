abacbc2b930e98dec44c2b652c8d558a3640f80f
nusratyusuf8458-ux <nusratyusuf8458@gmail.com>
2026-09-14 11:04:26 -0700

    phase-3a: mount Noor Daily experience

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './android.css'
 import './content.css'
+import './phase3.css'
 import App from './App'
 import Phase2Launcher from './Phase2Launcher'
 import Phase23Launcher from './Phase23Launcher'
 import GlobalSearchLauncher from './GlobalSearchLauncher'
+import Phase3Launcher from './Phase3Launcher'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /><GlobalSearchLauncher /></></StrictMode>,
+  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /><GlobalSearchLauncher /><Phase3Launcher /></></StrictMode>,
 )
