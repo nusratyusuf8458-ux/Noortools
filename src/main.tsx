6e8822bc3a33418942c485e849347ab6dbeaf36d
nusratyusuf8458-ux <nusratyusuf8458@gmail.com>
2026-09-14 07:45:55 -0700

    feat: expose Phase 2.3 source-gated library launcher

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import './android.css'
 import './content.css'
 import App from './App'
 import Phase2Launcher from './Phase2Launcher'
+import Phase23Launcher from './Phase23Launcher'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><><App /><Phase2Launcher /></></StrictMode>,
+  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /></></StrictMode>,
 )
