dd76a392508465f780bc8e6e6c310a685d5b9689
nusratyusuf8458-ux <nusratyusuf8458@gmail.com>
2026-09-14 11:16:56 -0700

    phase-3b: mount Phase 3B launcher

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import './styles.css'
 import './android.css'
 import './content.css'
 import './phase3.css'
+import './phase3b.css'
 import App from './App'
 import Phase2Launcher from './Phase2Launcher'
 import Phase23Launcher from './Phase23Launcher'
 import GlobalSearchLauncher from './GlobalSearchLauncher'
 import Phase3Launcher from './Phase3Launcher'
+import Phase3BLauncher from './Phase3BLauncher'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /><GlobalSearchLauncher /><Phase3Launcher /></></StrictMode>,
+  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /><GlobalSearchLauncher /><Phase3Launcher /><Phase3BLauncher /></></StrictMode>,
 )
