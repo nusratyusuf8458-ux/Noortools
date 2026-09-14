91ee42dd025036e90e0d4f4e6247e072bc90dea5
nusratyusuf8458-ux <nusratyusuf8458@gmail.com>
2026-09-14 08:33:10 -0700

    feat(2.4): mount premium search surface

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import './content.css'
 import App from './App'
 import Phase2Launcher from './Phase2Launcher'
 import Phase23Launcher from './Phase23Launcher'
+import GlobalSearchLauncher from './GlobalSearchLauncher'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /></></StrictMode>,
+  <StrictMode><><App /><Phase2Launcher /><Phase23Launcher /><GlobalSearchLauncher /></></StrictMode>,
 )
